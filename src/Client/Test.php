9173236fd95d2c4177ca071385636e8ee071aef9
748548395@qq.com <748548395@qq.com>
2021-01-09 10:42:58 +0800

    'test'

---
+++ src/Client/Test.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: houwei_public
+ * Date: 2021/1/9
+ * Time: 10:23
+ */
+namespace Client;
+class Test{
+    public static function test()
+    {
+        echo 'This is a package';
+    }
+}
